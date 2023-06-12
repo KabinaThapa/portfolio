b37d021422c1670c28d7715a4c366367b77bfe17
Kabina Thapa <thapakabina28@gmail.com>
2023-06-12 17:44:15 -0400

    React-type-animation setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import Sidenav from './components/sidenav'
-
+import Home  from './components/Home'
 
 function App() {
  
@@ -8,6 +8,7 @@ function App() {
     <>
     <div>
       <Sidenav />
+      <Home/>
       </div>
      
     </>
---
+++ src/components/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { TypeAnimation } from 'react-type-animation'
+
+const Home = () => {
+  return (
+    <div id="Home">Home</div>
+  )
+}
+
+export default Home
