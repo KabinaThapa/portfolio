bfb98e2cd8711dcf776eb3514b12deff8794f79a
Kabina Thapa <thapakabina28@gmail.com>
2023-09-26 16:33:37 -0400

    style: add resume layout in resume page

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
 
+
 const Resume = () => {
   return (
-    <div id="Resume">Resume</div>
+    <div className='border p-2 flex flex-col justify-center items-center h-screen' id="Resume">
+      <h1 className='text-3xl'>Resume</h1>
+      <div className='border h-96 w-96'>
+      <div className=''>Intro</div>
+      <div className=''>Skills</div>
+      <div className=''>Projects</div>
+      <div className=''>Experience</div>
+      </div>
+      
+    </div>
   )
 }
 
