cdae7f05b71ffa4582753af50ec3d6e7f102eea6
Kabina Thapa <thapakabina28@gmail.com>
2023-10-17 19:09:46 -0400

    fix:fix deployment issues

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,14 @@
 import React from 'react'
-import Home from './components/Home'
-import Sidenav from './components/sidenav'
-import Resume from './components/Resume'
-import Project from './components/Project'
-import Contact from './components/Contact'
+
 import MouseGlowEffect from './components/MouseGlow'
-import {Parallax, ParallaxLayer} from '@react-spring/parallax'
+import Sidenav from './components/sidenav'
 import ThreeScene from './components/three'
-import ParallaxScroll from './components/Parallax'
-import AnimatedBackground from './components/Parallax'
+
 import './App.css'
 
 function App ()  {
   return (
     <>
-    
-    
         <MouseGlowEffect/>
   <Sidenav/>
    
