086f72749373df8dd0f48f09edb8a52bde4d4ba1
Kabina Thapa <thapakabina28@gmail.com>
2023-10-17 19:04:46 -0400

    fix:fix deployment issues

--- src/main.jsx
+++ src/main.jsx
@@ -3,15 +3,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 import './index.css'
-import { createBrowserRouter, RouterProvider,Route } from 'react-router-dom'
-import Home from './components/Home.jsx'
-import Project from './components/Project.jsx'
-import Contact from './components/Contact.jsx'
-import Resume from './components/Resume.jsx'
-import Sidenav from './components/sidenav.jsx'
-import MouseGlowEffect from './components/MouseGlow'
 
-import ThreeScene from './components/three'
 import App from './App.jsx'
 
 
