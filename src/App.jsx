758cf46e61839a1024b89e28ec561490b34337b7
Kabina Thapa <thapakabina28@gmail.com>
2023-10-18 19:39:32 -0400

    fix:fix overscoll behaviour on mobile screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 
 import MouseGlowEffect from './components/MouseGlow'
 import Sidenav from './components/Sidenav'
@@ -7,6 +7,27 @@ import ThreeScene from './components/three'
 import './App.css'
 
 function App ()  {
+  useEffect(() => {
+    // Define the function to check window height changes
+    let lastHeight = window.innerHeight;
+
+    function checkWindowHeight() {
+      const newHeight = window.innerHeight;
+      if (newHeight !== lastHeight) {
+        // Window height has changed, indicating the address bar is hidden or shown.
+        // Perform any necessary adjustments to your layout.
+        lastHeight = newHeight;
+      }
+    }
+
+    // Check for window height changes periodically (e.g., every 500ms).
+    const heightCheckInterval = setInterval(checkWindowHeight, 500);
+
+    // Don't forget to clear the interval when the component unmounts.
+    return () => {
+      clearInterval(heightCheckInterval);
+    };
+  }, []); 
   return (
     <>
         <MouseGlowEffect/>
